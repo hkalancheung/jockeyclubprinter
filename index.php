fd22b6f03f5a50c6a341d4c942f0237b6aa31de0
Samuel Kindler <sam@flamov.com>
2014-10-10 18:25:15 +0800

    int commt

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="user-scalable=no" />
+		<title>Jockey Club</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+		<script type="text/javascript" src="js/scripts_global.js"></script>
+	</head>
+
+	<body>
+
+		
+
+
+
+
+
+	<div id="overlay" class="invisible">
+
+		<div id="overlay-wrapper">
+
+			<div id="overlay-image"></div>
+
+			<div id="overlay-confirm" class="overlay-button"><a href="#">Print</a></div>
+			<div id="overlay-cancel" class="overlay-button"><a href="#">Cancel</a></div>
+
+		</div>
+
+	</div>
+
+
+
+
+	<div id="topbar">
+
+		
+
+	</div>
+
+
+
+
+
+
+	<div id="feed">
+		
+		<ul>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+			<li style="background-image: url('http://placekitten.com/500/250');"><a href="http://placekitten.com/500/250"></a></li>
+		</ul>
+
+		<div class="cf"></Div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+	</body>
+
+</html>
